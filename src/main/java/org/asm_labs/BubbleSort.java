18dc056e6e41a1fc2660cc633f86989e3c516aad
ymaslov <ymaslov@ispras.ru>
2017-06-28 14:17:16 +0300

    Added javadocs for BubbleSort.java and FileCreator.java

--- src/main/java/org/asm_labs/BubbleSort.java
+++ src/main/java/org/asm_labs/BubbleSort.java
@@ -3,19 +3,29 @@ package org.asm_labs;
 import java.io.*;
 import java.util.Comparator;
 
-/**
+/**Класс, реализующий интерфейс Sort
+ * Этот класс реализует пузырьковую сортировку
+ * У данного класса одно состояние <b>comparator</b>
  * @author drunkgranny
  *         Date: 23.06.17.
+ * @version 1.0
+ * @see Sort<T>#sort(RandomAccessable<T> randomAccessable)
  */
-
 public class BubbleSort<T> implements Sort<T> {
 
+    /** Поле comparator */
     private Comparator<T> comparator;
 
+    /** Конструктор - создание нового объекта с определенными значениями
+     * @param comparator - компаратор
+     */
     public BubbleSort(Comparator<T> comparator) {
         this.comparator = comparator;
     }
 
+    /** Метод реализующий интерфейс Sort
+     * @see Sort<T>#sort(RandomAccessable<T> randomAccessable)
+     */
     @Override
     public void sort(RandomAccessable<T> randomAccessable) throws IOException {
 
@@ -25,7 +35,7 @@ public class BubbleSort<T> implements Sort<T> {
             while (flag)
             {
                 flag= false;    //set flag to false awaiting a possible swap
-                for( j = 0;  j < randomAccessable.size() - 1;  j++ )
+                for(j = 0;  j < randomAccessable.size() - 1;  j++)
                 {
                     if (comparator.compare(randomAccessable.get(j), randomAccessable.get(j + 1)) > 0)
                     {
--- src/main/java/org/asm_labs/FileCreator.java
+++ src/main/java/org/asm_labs/FileCreator.java
@@ -2,17 +2,30 @@ package org.asm_labs;
 
 import java.io.*;
 
-/**
- * Created by ymaslov on 22.06.17.
+/**Класс реализует создание файла
+ * Файл содержит N строк длинной 12 символов + 1 символ '\n'
+ * У данного класса одно состояние <b>fileName</b>
+ * @author drunkgranny
+ *         Date: 23.06.17.
+ * @version 1.0
  */
 public class FileCreator {
 
+    /** Поле имени файла */
     private String fileName = null;
 
+    /** Конуструктор - создание нового объекта с определенными значениями
+     * @param fileName - имя файла
+     */
     public FileCreator(String fileName) {
         this.fileName = fileName;
     }
 
+    /**
+     * Метод создания файла
+     * @return файл
+     * @throws IOException
+     */
     public File createFile() throws IOException {
         File file = new File(fileName);
         if (file.createNewFile()) {
@@ -23,6 +36,11 @@ public class FileCreator {
         return file;
     }
 
+    /**
+     * Метод заполнения файла
+     * @param file - файл
+     * @throws IOException
+     */
     public void fillTheFile(File file) throws IOException {
         long start = System.currentTimeMillis();
         Writer fw = new BufferedWriter(new FileWriter(file), 2 * 2048);
@@ -34,13 +52,6 @@ public class FileCreator {
         fw.close();
         long elapsedTime = (System.currentTimeMillis() - start) / 1000; // count in Millis
         System.out.println(elapsedTime);
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        FileCreator file = new FileCreator(args[0]);
-        File myFile = file.createFile();
-        file.fillTheFile(myFile);
     }
 
 }
