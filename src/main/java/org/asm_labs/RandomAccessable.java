d6b166ae3b09741013ff7104ef3f6def727284e2
drunkgranny <drunkmowgli@gmail.com>
2017-06-28 14:49:30 +0300

    Added javadocs for RandomAccessable.java and Sort.java

--- src/main/java/org/asm_labs/RandomAccessable.java
+++ src/main/java/org/asm_labs/RandomAccessable.java
@@ -2,16 +2,43 @@ package org.asm_labs;
 
 import java.io.IOException;
 
-/**
+/** Итерфейс произвольного доступа к структуре данных любого типа
  * @author drunkgranny
  *         Date: 23.06.17.
  */
 
 public interface RandomAccessable<T> {
 
+    /** Метод получения произвольного элемента по индексу
+     *
+     * @param index индекс элемента, который надо получить
+     * @return элемент
+     * @throws IOException
+     */
     T get(int index) throws IOException;
+
+    /**
+     * Метод получения количество элементов в структуре данных
+     * @return количество элементов
+     * @throws IOException
+     */
     int size() throws IOException;
+
+    /**
+     * Метод записи элемента в структуру данных по индексу
+     * @param index на какую позицию записать элемент
+     * @param element какой элемент записать
+     * @throws IOException
+     */
     void set(int index, T element) throws IOException;
+
+    /**
+     * Метод обмена элементами
+     * @param index1 первый элемент
+     * @param index2 второй элемент
+     * @param randomAccessable структура данных, содержащая элементы
+     * @throws IOException
+     */
     default void swap(int index1, int index2, RandomAccessable<T> randomAccessable) throws IOException {
         T temp;
         temp = randomAccessable.get(index1);
--- src/main/java/org/asm_labs/Sort.java
+++ src/main/java/org/asm_labs/Sort.java
@@ -2,13 +2,18 @@ package org.asm_labs;
 
 import java.io.IOException;
 
-/**
+/** Интерфейс сортировки для любых любых данных
  * @author drunkgranny
  *         Date: 23.06.17.
  */
 
 public interface Sort<T> {
 
+    /** Метод сортировки элементов
+     *
+     * @param randomAccessable объект содержащий элементы для сортировки
+     * @throws IOException
+     */
     void sort(RandomAccessable<T> randomAccessable) throws IOException;
 
 }
