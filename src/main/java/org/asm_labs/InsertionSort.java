7f5b059aa04f7442c2bb5897a7030a56ffe3b5cf
drunkmowgli <drunkmowgli@gmail.com>
2017-07-05 10:39:28 +0300

    Added InsertionSort.

---
+++ src/main/java/org/asm_labs/InsertionSort.java
@@ -0,0 +1,31 @@
+package org.asm_labs;
+
+import java.io.IOException;
+import java.util.Comparator;
+
+/**
+ * Created by 217dr on 03.07.2017.
+ */
+public class InsertionSort<T> implements Sort<T> {
+
+    private Comparator<T> comparator;
+
+    public InsertionSort(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
+
+    @Override
+    public void sort(RandomAccessable<T> randomAccessable) throws IOException {
+        int out, in;
+
+        for (out = 1; out < randomAccessable.size(); out++) {
+            T temp = randomAccessable.get(out);
+            in = out;
+            while (in > 0 && comparator.compare(randomAccessable.get(in - 1), temp) >= 0) {
+                randomAccessable.set(in, randomAccessable.get(in - 1));
+                --in;
+            }
+            randomAccessable.set(in, temp);
+        }
+    }
+}
